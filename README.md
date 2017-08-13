a3a97738c59a904d3ae2e7f86730dacd9bb118cb
Jessica Leach <jessicamaryleach@gmail.com>
2017-08-13 22:27:50 +0200

    readme added

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # randimal-extension
+
+This is a chrome extension to generate a random animal to brighten your day. 
+
+Make a pull request to if you would like to contribute!
--- popup.js
+++ popup.js
@@ -4,22 +4,14 @@ window.onload = function() {
 
     var button = document.getElementById("submit");
 
-    var random = Math.floor(Math.random() * (3 - 0 + 0) + 0);
-
     button.addEventListener("click", function() {
 
         var answer = document.getElementById("img");
 
         var random = Math.floor(Math.random() * (3 - 0 + 0) + 0);
 
-        console.log(imageList[random]);
-
-        var randomImg = imageList[random];
-
-        console.log(randomImg);
         answer.setAttribute("src", imageList[random]);
 
-        console.log(answer)
 
     });
 }
