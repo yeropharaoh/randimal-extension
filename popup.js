2f531c2076b16530dd285b1adf2700f6f19cd597
yeropharaoh <yeroschutte@gmail.com>
2017-08-17 12:28:29 +0200

    yero's pics

---
+++ popup.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+
+    var imageList = ["img/1.jpg", "img/2.jpg", "img/3.jpg", "img/4.jpg"]
+
+    var button = document.getElementById("submit");
+
+    button.addEventListener("click", function() {
+
+        var answer = document.getElementById("img");
+
+        var random = Math.floor(Math.random() * (3 - 0 + 0) + 1);
+
+        answer.setAttribute("src", imageList[random]);
+
+
+    });
+}
